15d342359171cc529711e68de2bc9a3f9e081470
brianthungu <brianthungu.bt@gmail.com>
2023-04-04 22:31:19 +0300

    everything

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 class LockedClass:
-    """A locked class that only lets the user dynamically create the instance
-    attribute 'first_name'"""
     __slots__ = ['first_name']
+
